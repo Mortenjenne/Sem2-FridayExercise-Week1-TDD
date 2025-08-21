09d572f1209f7127e14b8b63e661b522de5906ba
Morten Jensen <mo_ten@hotmail.com>
2025-08-21 19:13:25 +0200

    Add test first test passed

--- src/main/java/io/github/mortenjenne/tdd/Calculator.java
+++ src/main/java/io/github/mortenjenne/tdd/Calculator.java
@@ -1,4 +1,8 @@
 package io.github.mortenjenne.tdd;
 
 public class Calculator {
+
+    public int add(int a, int b) {
+        return 4;
+    }
 }
--- src/test/java/io/github/mortenjenne/tdd/CalculatorTest.java
+++ src/test/java/io/github/mortenjenne/tdd/CalculatorTest.java
@@ -2,10 +2,14 @@ package io.github.mortenjenne.tdd;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+
+    private Calculator calculator = new Calculator();
 
     @BeforeEach
     void setUp() {
@@ -13,5 +17,13 @@ class CalculatorTest {
 
     @AfterEach
     void tearDown() {
+    }
+
+    @DisplayName("Test add method works")
+    @Test
+    void testTwoPlusTwo(){
+        int expected = 4;
+        int actual = calculator.add(2,2);
+        assertEquals(2,2);
     }
 }
